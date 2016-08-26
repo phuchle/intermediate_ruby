e48c50600e1c74044a9be26d0fed58fd503ef2ff
Phuc Le <le.phuc.h@gmail.com>
2016-08-25 22:36:55 -0700

    Expand logic for board and players.  Remove go first logic to simplify testing.  Begin writing game instructions.  Still needs to loop until a win condition is met and then declare the winner.

--- oop_projects/tictactoe.rb
+++ oop_projects/tictactoe.rb
@@ -14,20 +14,28 @@
 
 class Tictactoe
   attr_accessor :board :player1 :player2
+  attr_reader :go_first
 
   def initialize(player1, player2)
     @player1 = Player.new(player1)
     @player2 = Player.new(player2)
     @board = [
-      %w(a _ _ _)\n
-      %w(b _ _ _)\n
-      %w(c _ _ _)\n
+      %w(0 1 2 3),
+      %w(1 _ _ _),
+      %w(2 _ _ _),
+      %w(3 _ _ _),
     ]
-    @go_first = rand(10) <= 5 ? @player1 : @player2
+
   end
 
-  class Player(player_name)
+  def board
+    puts @board[0].to_s
+    puts @board[1].to_s
+    puts @board[2].to_s
+    puts @board[3].to_s
+  end
 
+  class Player
     def initialize(player_name)
       @player_name = player_name
     end
@@ -38,3 +46,23 @@ class Tictactoe
 
   end
 end
+
+puts "Welcome to Tic-Tac-Toe."
+puts "Please enter the first player's name:"
+player1 = gets.chomp
+puts "Thank you.  Player one is #{player1}."
+
+puts "Please enter player two's name:"
+player2 = gets.chomp
+puts "Thank you.  Player two is #{player2}."
+
+game = Tictactoe.new(player1, player2)
+
+puts @board
+puts "Enter the board coordinates you wish to mark"
+puts "e.g. 1,3 marks the bottom left corner of the board."
+
+
+puts "#{player1}, enter the coordinates you wish to mark:"
+x_coord, y_coord = gets.chomp("x, y")
+puts @board
