915f7687ae1edd5ebc2d145f3b9a3c9ba81d6cd8
Phuc Le <le.phuc.h@gmail.com>
2016-08-30 20:08:12 -0700

    Finish win condition methods.  Finish writing text for play-through.  Refine logic for getting and setting moves on the bord.  Game is playable.

--- oop_projects/tictactoe.rb
+++ oop_projects/tictactoe.rb
@@ -1,23 +1,17 @@
-# one player is x and one player is o
-# the board is displayed between each person's turn
-# the board has to change based on players' inputs
-#
-# players can be a class - there will be 2 of them
-# players need method that will access the 2D array
-# players exist inside the board class
-# the board can have x value 1...3, and y value a...c
-#   1 2 3
-# a _ _ _
-# b _ _ _
-# c _ _ _
-# the above is a 2D array
-# the game ends when a player has connected a row or column
-# the game also ends when diagonal is filled
-# the game can end when all tiles are filled with no winner
-#
+class Array
+  def filled?
+    if self.all? { |char| char == "x"}
+      true
+    elsif self.all? { |char| char == "o"}
+      true
+    else
+      false
+    end
+  end
+end
 
 class Tictactoe
-  # attr_accessor :board, :player1, :player2
+  attr_accessor :board, :demo_board, :player1, :player2
 
   def initialize(player1, player2)
     @player1 = Player.new(player1)
@@ -47,38 +41,38 @@ class Tictactoe
     puts @board[2].join(" ")
   end
 
+  def demo_board
+    puts @demo_board[0].join(" ")
+    puts @demo_board[1].join(" ")
+    puts @demo_board[2].join(" ")
+    puts @demo_board[3].join(" ")
+    puts "\n\n"
+  end
+
   def win?
-    rows = @board
-
-    columns = []
-
-    index = 0
-    while index < rows.length - 1
-      columns << rows.collect { |row| row[index] }
-      index += 1
-    end
-
-    diagonal1 = [ @board[0][0], @board[1][1], @board[2][2] ]
+    rows = @board #2D array
+    columns = [] #2D array
+    0.upto(2) { |index| columns << rows.collect { |row| row[index] } }
+    diagonal1 = [ @board[0][0], @board[1][1], @board[2][2] ] #1D arr
     diagonal2 = [ @board[0][2], @board[1][1], @board[2][0] ]
-    diagonals = [diagonal1, diagonal2]
+    diagonals = [diagonal1, diagonal2] #2D array
 
-    straight_lines = [rows, columns, diagonals] # how many dimensions is dis??
+    straight_lines = [rows, columns, diagonals] # 3D array
 
-    true if straight_lines.any? { |line| line.filled? }
-    false
+    win_condition = false
+    straight_lines.each do |straight_line|
+      win_condition = true if straight_line.any? { |line| line.filled? }
+    end
+    win_condition
   end
 
   def tie?
-    self.board.flatten.none? { |char| char == "_" }
-  end
-
-  def filled?
-    self.all? { |char| char == "x" || char == "o" }
+    @board.flatten.none? { |char| char == "_" }
   end
 
   def move(current_player, x_coord, y_coord)
-    @board[x_coord][y_coord] = current_player == @player1 ? "x" : "o"
-    self.board
+      @board[x_coord][y_coord] = current_player == @player1 ? "x" : "o"
+      self.board
   end
 
   class Player
@@ -90,45 +84,81 @@ class Tictactoe
   end
 end
 
+Gem.win_platform? ? (system "cls") : (system "clear")
 
 puts "==========================="
 puts "  Welcome to Tic-Tac-Toe.\n"
 puts "==========================="
 
-puts "Please enter the first player's name:\n"
+sleep 1
+
+puts "Please enter the player one's name:\n"
 player1 = gets.chomp
-puts "Thank you.  Player one is #{player1}.\n\n"
+puts "Thank you."
+sleep 1
+puts "Player one is #{player1}.\n\n"
+sleep 1
 
 puts "Please enter player two's name:"
 player2 = gets.chomp
-puts "Thank you.  Player two is #{player2}.\n\n"
+puts "Thank you."
+sleep 1
+puts "Player two is #{player2}.\n\n"
+sleep 1
+
+puts "A new game will now begin!"
+sleep 2
+
+Gem.win_platform? ? (system "cls") : (system "clear")
 
 game = Tictactoe.new(player1, player2)
+sleep 2
 
-puts game.demo_board + "\n"
+puts "The board with coordinates is shown below:"
+sleep 2
+puts game.demo_board
+sleep 2
+
 puts "Enter the board coordinates you wish to mark.\n"
-puts "e.g. 1,3 marks the bottom left corner of the board.\n"
+puts "e.g. 1,3 marks the bottom left corner of the board.\n\n\n"
+sleep 3
+
+sleep 1
 
 game_ends = false
 
+game.board
+
+idx = 1
 until game_ends
-  idx = 1
-  current_player = idx.odd? ? @player1 : @player2
+  current_player = idx.odd? ? game.player1 : game.player2
+  puts "#{current_player.name}, enter the coordinates you wish to mark (1-3):"
+  coords = gets.chomp.split(",").map!(&:to_i)
+  x_coord, y_coord = coords[0] - 1, coords[1] - 1 # since array starts at 0
 
-  puts "#{current_player}, enter the coordinates you wish to mark:"
-  x_coord, y_coord = gets.chomp
-
-  game.move(current_player, x_coord - 1, y_coord - 1) # since array starts at 0
+  game.move(current_player, x_coord, y_coord)
 
   if game.win?
-    puts "#{current_player} wins!"
+    Gem.win_platform? ? (system "cls") : (system "clear")
+    sleep 1
+    10.times do
+      puts "==========================="
+      puts "#{current_player.name} wins!"
+      puts "==========================="
+      sleep 0.25
+    end
+    sleep 2
     game_ends = true
   elsif game.tie?
-    puts "Nobody wins."
+    puts "==========================="
+    puts "       Nobody wins."
+    puts "==========================="
     game_ends = true
+  else
+    game_ends = false
   end
-
   idx += 1
 end
 
+Gem.win_platform? ? (system "cls") : (system "clear")
 puts "Thank you for playing Tic-Tac-Toe!"
